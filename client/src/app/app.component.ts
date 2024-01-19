e3de748cdadc14ff239e316cc33335ffb8aceead
Mustafa Ulaş <mustafaulas@windowslive.com>
2024-01-19 16:56:16 +0300

    Angular Navbar has added , Products has beeg got from API

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Bookhub';
+  products:Product[]=[];
+
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.http.get<Product>('https://localhost:5001/api/products').subscribe({
+      next: response => this.products,//what to do next
+      error: error => console.log("eror:" + error),//what to do if there is error
+      complete: () => {
+        console.log("Requested completed")
+      }
+    })
+  }
 }

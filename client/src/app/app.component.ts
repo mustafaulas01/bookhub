06a983099de8bbfb19122ac870a13353f0149ead
Mustafa Ulaş <mustafaulas@windowslive.com>
2024-01-23 16:44:46 +0300

    Angular Shopping services,rouing component and Modules have been added.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Product } from './models/product';
+import { Product } from './shared/models/product';
 
 @Component({
   selector: 'app-root',
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,20 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
 import {HttpClientModule} from '@angular/common/http';
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]

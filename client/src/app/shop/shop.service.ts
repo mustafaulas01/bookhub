571b7842e1f54dcdb3a91da96212b45ef32e01a9
Mustafa Ulaş <mustafaulas@windowslive.com>
2024-01-23 16:52:39 +0300

    Shop Services has been added in Angular Client

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Product } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  baseUrl="https://localhost/5001/api/"
+
+  getProducts()
+  {
+    return this.http.get<Product[]>(this.baseUrl+'products');
+  }
 }
